ac2bd70e12efe21a03ff2dfab5655e017c7755fd
Ozodbek <ozodbekprimov69@gmail.com>
2025-01-26 11:25:42 +0500

    initial project

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import main, person_list
+urlpatterns = [
+    path('', main, name='main'),
+    path('person-list/', person_list, name='person_list' ),
+]
+
